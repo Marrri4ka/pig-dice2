0cbc416b19bea88a1d8c1f66fbf8af2df7475d5b
Mariia <mariiapopovych@gmail.com>
2019-04-10 19:41:17 -0700

    add levels-buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
-
-var player1 = new Player ("Anna");
-var player2 = new Player ("Mariia");
+var player1 = new Player("Anna");
+var player2 = new Player("Mariia");
 var player1Turn = true;
 var randomNumber;
 var score = 0;
@@ -8,33 +7,31 @@ var score = 0;
 
 
 
-function Player (name) {
+function Player(name) {
   this.name = name,
-  this.score = 0
+    this.score = 0
 }
 
 
 function roll() {
-  randomNumber = Math.floor(Math.random() *6) +1;
+  randomNumber = Math.floor(Math.random() * 6) + 1;
   console.log(randomNumber);
-  if (randomNumber === 1){
+  if (randomNumber === 1) {
     score = 0;
     passTurn();
-  }
-  else {
+  } else {
     score += randomNumber; //This adds up the score
   }
 }
 
-function passTurn () {
+function passTurn() {
   if (player1Turn) {
     player1.score += score; //This adds the die score to the Player's constructor score
-    player1Turn =! player1Turn; //Saying this is "not" Player1's turn, so "true" turns to "false"
+    player1Turn = !player1Turn; //Saying this is "not" Player1's turn, so "true" turns to "false"
     winner();
     alert("Player2, it's your turn!");
-  }
-  else {
-  player1Turn =! player1Turn; //signifies player2turn (player1Turn = "false")
+  } else {
+    player1Turn = !player1Turn; //signifies player2turn (player1Turn = "false")
     player2.score += score;
     winner();
     alert("Player1, it's your turn!");
@@ -43,12 +40,11 @@ function passTurn () {
 
 function winner() {
   if (player1.score > 10) {
-    $("#image1").attr("src","img/winner.jpg");
+    $("#image1").attr("src", "img/winner.jpg");
     $("#pig-dice").slideUp();
-  }
-  else if (player2.score > 10) {
-    $("#image1").attr("src","img/winner.jpg");
-      $("#pig-dice").slideUp();
+  } else if (player2.score > 10) {
+    $("#image1").attr("src", "img/winner.jpg");
+    $("#pig-dice").slideUp();
   }
 }
 
@@ -58,31 +54,32 @@ $(document).ready(function() {
     //  $("#roll-area").text(randomNumber);
     //   $("#roll-area").show();
 
-          $("#image").attr("src","img/"+ randomNumber + ".png");
+    $("#image").attr("src", "img/" + randomNumber + ".png");
 
 
-            //  $("#roll-area").attr(src="img/+randomNumber + ".png"");
+    //  $("#roll-area").attr(src="img/+randomNumber + ".png"");
 
   });
   $("#pass-turn").click(function() {
     passTurn(); //the function that runs once the Pass-turn button is clicked
     $("#player1-area").text(player1.score);
-      $("#player1-area").show();
+    $("#player1-area").show();
 
     $("#player2-area").text(player2.score);
-      $("#player2-area").show();
+    $("#player2-area").show();
   });
-  $("#play").click(function(event){
+  $("#play").click(function(event) {
     event.preventDefault();
 
 
     $(".names").slideUp();
-    $("#pig-dice").slideDown();
-    var name1 = $("input#username1").val();
-    var name2 = $("input#username2").val();
-    $("#player-one").text(name1);
-    $("#player-one").show(name1);
-    $("#player-two").text(name2);
-    $("#player-two").show(name2);
+    // $("#pig-dice").slideDown();
+    $(".levels").slideDown();
+    // var name1 = $("input#username1").val();
+    // var name2 = $("input#username2").val();
+    // $("#player-one").text(name1);
+    // $("#player-one").show(name1);
+    // $("#player-two").text(name2);
+    // $("#player-two").show(name2);
   });
 });
